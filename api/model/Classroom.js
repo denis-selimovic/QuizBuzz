b620354f65618faebc3ba882fcca5b3366f26fea
dselimovic <dselimovic1@etf.unsa.ba>
2020-10-16 15:31:53 +0200

    added remove hook to Classroom model

--- api/model/Classroom.js
+++ api/model/Classroom.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const shortid = require('shortid');
 const { Schema } = mongoose;
+const Quiz = require('Quiz');
 
 const classroomSchema = new Schema({
   code: {
@@ -19,11 +20,12 @@ const classroomSchema = new Schema({
       type: Schema.Types.ObjectId,
       ref: "Student",
     },
-  ],
+  ]
 });
 
 classroomSchema.pre('remove', async function (next) {
-
+  const quizzes = this.quizzes.map(q => q._id);
+  await Quiz.deleteMany({ _id: { $in: quizzes } });
 });
 
 classroomSchema.statics.getClassroomByIdAndPopulate = async (
