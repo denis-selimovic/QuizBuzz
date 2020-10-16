1c8a8441270121487fc4e4bf567468d28d71f53c
lvrnjak1 <lvrnjak1@etf.unsa.ba>
2020-10-16 12:07:59 +0200

    body validator middleware

---
+++ api/common/http.js
@@ -0,0 +1,28 @@
+// const partialBodyValidator = (body, validBody) => {
+//   return body.every((value) => validBody.includes(value));
+// };
+
+const validateBody = (validBody) => {
+  return (req, res, next) => {
+    const body = Object.keys(req.body);
+    if (validBody.every((value) => body.includes(value))) {
+      return next();
+    }
+    res.status(403).json({ message: "Invalid body" });
+  };
+};
+
+const partiallyValidateBody = (validBody) => {
+  return (req, res, next) => {
+    const body = Object.keys(req.body);
+    if (body.every((value) => validBody.includes(value))) {
+      return next();
+    }
+    res.status(403).json({ message: "Invalid body" });
+  };
+};
+
+module.exports = {
+  validateBody,
+  partiallyValidateBody,
+};
