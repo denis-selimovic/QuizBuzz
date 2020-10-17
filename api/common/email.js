cd6a7a8e49920674c116a42c150ac2229d45268a
dselimovic <dselimovic1@etf.unsa.ba>
2020-10-17 18:38:25 +0200

    Added mail util

---
+++ api/common/email.js
@@ -0,0 +1,12 @@
+const sendEmail = async (to, subject, text) => {
+    const send = require('gmail-send')({
+        user: process.env.QUIZ_BUZZ_MAIL,
+        pass: process.env_QUIZ_BUZZ_PASS,
+        to,
+        subject,
+        text
+    });
+    return await send();
+};
+
+module.exports = sendEmail;
