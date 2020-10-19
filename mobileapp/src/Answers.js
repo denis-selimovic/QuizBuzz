233029fedbed24749a3c82d2a5c5d615193284e4
dselimovic <dselimovic1@etf.unsa.ba>
2020-10-19 23:45:30 +0200

    Added icons

--- mobileapp/src/Answers.js
+++ mobileapp/src/Answers.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet, ScrollView } from 'react-native';
+import { View, StyleSheet, ScrollView, Image } from 'react-native';
 import { Checkbox } from "@ant-design/react-native";
 
 export default (props) => {
@@ -9,27 +9,34 @@ export default (props) => {
         return selected.includes(id);
     };
 
+    const getIcon = a => {
+        return a.correct ? require("../src/images/correct.png") : require("../src/images/incorrect.png");
+    }
+
     const getCheckbox = (a) => {
         if (!readonly) {
             return (
-                <Checkbox key={a._id} defaultChecked={setChecked(a._id)} onChange={params => changeAnswer(params, a._id)}>
-                    {a.content}
-                </Checkbox>
+                <View key={a._id} style={styles.answer}>
+                    <Checkbox key={a._id} defaultChecked={setChecked(a._id)} onChange={params => changeAnswer(params, a._id)}>
+                        {a.content}
+                    </Checkbox>
+                </View>
             )
         }
         return (
-            <Checkbox key={a._id} checked={setChecked(a._id)} disabled={true}>
-                {a.content}
-            </Checkbox>
+            <View key={a._id} style={styles.answerWithIcon}>
+                <View style={styles.checkboxContainer}>
+                    <Checkbox key={a._id} checked={setChecked(a._id)} disabled={true}>{a.content}</Checkbox>
+                </View>
+                <View style={styles.imgContainer}>
+                    <Image style={styles.image} source={getIcon(a)}/>
+                </View>
+            </View>
         );
     }
 
     const renderAnswers = () => {
-        return answers.map(a => {
-            return (
-                <View key={a._id} style={styles.answer}>{getCheckbox(a)}</View>
-            );
-        });
+        return answers.map(a => getCheckbox(a));
     };
 
     const changeAnswer = (params, answerId) => {
@@ -56,5 +63,30 @@ const styles = StyleSheet.create({
         flexGrow: 1,
         marginTop: 20,
         maxHeight: 380
+    },
+    image: {
+        maxWidth: 25,
+        maxHeight: 25
+    },
+    answerWithIcon: {
+        height: 70,
+        paddingLeft: 10,
+        paddingRight: 10,
+        minWidth: 300,
+        display: 'flex',
+        flexDirection: 'row',
+    },
+    imgContainer: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 70,
+        minWidth: 50
+    },
+    checkboxContainer: {
+        minWidth: 250,
+        height: 70,
+        display: 'flex',
+        justifyContent: 'center'
     }
 })
