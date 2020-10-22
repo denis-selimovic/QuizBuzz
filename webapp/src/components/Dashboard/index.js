0a050e50d35cd5390345da245a72f7395938c6e2
dselimovic <dselimovic1@etf.unsa.ba>
2020-10-22 17:55:25 +0200

    First

--- webapp/src/components/Dashboard/index.js
+++ webapp/src/components/Dashboard/index.js
@@ -1,11 +1,45 @@
 import React from "react";
-import './classroom.css'
+import './dashboard.css'
+import { Menu } from "antd";
+
+const { Item } = Menu;
+
+const menuStyle = {
+    backgroundColor: 'black',
+    color: 'white',
+    fontSize: '16px'
+}
 
 class Dashboard extends React.Component {
+
+    state = {
+        current: 'classroom'
+    }
+
+    constructor(props) {
+        super(props);
+    }
+
+    handleClick = e => {
+        this.setState({ current: e.key });
+    }
+
     render() {
         return (
-            <div>
-                Dashboard
+            <div className="dashboard">
+                <div className="header">
+                    <div className="left-menu">
+                        <Menu selectedKeys={[this.state.current]} onClick={this.handleClick} mode={`horizontal`} theme={`dark`} style={menuStyle}>
+                            <Item key={`classroom`}>Classrooms</Item>
+                            <Item key={`quiz`}>Quizzes</Item>
+                        </Menu>
+                    </div>
+                    <div className="right-menu">
+                        <div className="item">
+                            Classrooms
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
