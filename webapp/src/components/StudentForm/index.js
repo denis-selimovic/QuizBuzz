02ed2036aee4dee15ffe511c95d28f26fb13cb95
dselimovic <dselimovic1@etf.unsa.ba>
2020-10-22 21:48:58 +0200

    added form for adding students

--- webapp/src/components/StudentForm/index.js
+++ webapp/src/components/StudentForm/index.js
@@ -1,5 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
+import { withRouter } from 'react-router-dom';
+import { Form, Input, Button, Card } from "antd";
+import axios from 'axios';
+import './studentForm.css';
 
-export default (props) => {
+import TOKEN from "../../token";
 
+const formItemLayout = {
+    labelCol: {
+        xs: { span: 24 },
+        sm: { span: 10 },
+    },
+    wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 16 },
+    },
 };
+const tailFormItemLayout = {
+    wrapperCol: {
+        xs: {
+            span: 30,
+            offset: 0,
+        },
+        sm: {
+            span: 30,
+            offset: 10,
+        },
+    },
+};
+
+const StudentForm = (props) => {
+
+    const [form] = Form.useForm();
+
+    const onFinish = async form => {
+        const classroom = props.location.state.record;
+        if (!classroom) return;
+        await axios.post(`https://quiz-buzz-api.herokuapp.com/classrooms/${classroom.key}/student`, form, {
+            headers: {
+                Authorization: 'Bearer ' + TOKEN
+            }
+        });
+        const { history } = props;
+        history.push({
+            pathname: '/students',
+            state: { record: classroom }
+        });
+    }
+
+    const onFinishFailed = e => {
+
+    }
+
+    return (
+        <div className="form-container">
+            <Card className="form">
+                <Form form={form} {...formItemLayout} name="register" initialValues={{ remember: true }}
+                      onFinish={onFinish} onFinishFailed={onFinishFailed}>
+
+                    <Form.Item label="Email" name="email"
+                               rules={[{ required: true, message: 'Please input your email!' }]}>
+                        <Input />
+                    </Form.Item>
+
+                    <Form.Item label="Name" name="name"
+                               rules={[{ required: true, message: 'Please input your name!' }]}>
+                        <Input />
+                    </Form.Item>
+
+                    <Form.Item label="Surname" name="surname"
+                               rules={[{ required: true, message: 'Please input your surname!' }]}>
+                        <Input />
+                    </Form.Item>
+
+                    <Form.Item {...tailFormItemLayout}>
+                        <Button type="primary" htmlType="submit"> Register </Button>
+                    </Form.Item>
+                </Form>
+            </Card>
+        </div>
+    );
+}
+
+export default withRouter(StudentForm);
